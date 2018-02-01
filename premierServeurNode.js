3f53b417e392115387c69b9871edda937148f629
valp26 <valp26@outlook.com>
2018-02-01 11:49:58 -0500

    Création d'un serveur local Web avec Node.js

---
+++ premierServeurNode.js
@@ -0,0 +1,8 @@
+var http = require("http"); 
+http.createServer((request, response) =>
+{ 
+ console.log('branchement sur le port 3000')
+ response.writeHead(200, {"Content-Type": "text/plain"});
+ response.write("Hello World"); 
+ response.end(); 
+}).listen(3000);
