acdb6f8e26634d81d64d6ea3cecde7dcdb7b04ae
valp26 <valp26@outlook.com>
2018-02-06 13:39:17 -0500

    Correction du code pour l'affichage des provinces

--- premierServeurNode.js
+++ premierServeurNode.js
@@ -2,13 +2,14 @@ var http = require("http");
 http.createServer((request, response) =>
 { 
  console.log('branchement sur le port 3000')
- response.writeHead(200, {"Content-Type": "text/html"});
+ response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
  const fs = require("fs");
  let data = fs.readFileSync('provinces.json');
+ let tt = JSON.parse(data);
  let tableau = '<table>';
  console.log(data);
- for (let p in data) {
- 	tableau += '<tr><td>' + p + '</td><td>' + data[p] + '</td></tr>';
+ for (let p in tt) {
+ 	tableau += '<tr><td>' + p + '</td><td>' + tt[p] + '</td></tr>';
  }
  tableau += '</table>';
  response.write(tableau);
