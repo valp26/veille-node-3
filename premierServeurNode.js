d43476a8f64429e5e8635dd90e3c8739bcdfa5e2
valp26 <valp26@outlook.com>
2018-02-06 13:29:09 -0500

    Ajout du code pour afficher les provinces

--- premierServeurNode.js
+++ premierServeurNode.js
@@ -2,7 +2,15 @@ var http = require("http");
 http.createServer((request, response) =>
 { 
  console.log('branchement sur le port 3000')
- response.writeHead(200, {"Content-Type": "text/plain"});
- response.write("Hello World"); 
+ response.writeHead(200, {"Content-Type": "text/html"});
+ const fs = require("fs");
+ let data = fs.readFileSync('provinces.json');
+ let tableau = '<table>';
+ console.log(data);
+ for (let p in data) {
+ 	tableau += '<tr><td>' + p + '</td><td>' + data[p] + '</td></tr>';
+ }
+ tableau += '</table>';
+ response.write(tableau);
  response.end(); 
 }).listen(3000);
